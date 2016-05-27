e74e16b18fc0ff8a3c129732e7e9a9cc0bf5dd53
Emanuelle CS Flor <manu714074@gmail.com>
2016-05-27 17:20:04 -0300

    Teste master

--- EscolaIdiomas/FormCadastrarAluno.cs
+++ EscolaIdiomas/FormCadastrarAluno.cs
@@ -20,26 +20,6 @@ namespace EscolaIdiomas
 
         private void btn_salvarAluno_Click(object sender, EventArgs e)
         {
-            /*if (Aluno.VerificaRG(msk_rgALuno.Text.Trim()))
-                MessageBox.Show("OK rg");
-            else
-                MessageBox.Show("Erro rg");
-
-            if (Aluno.VerificaCPF(msk_cpfAluno.Text.Trim()))
-                MessageBox.Show("OK cpf");
-            else
-                MessageBox.Show("Erro cpf");
-
-            if (Aluno.VerificaEmail(txt_emailAluno.Text.Trim()))
-                MessageBox.Show("OK email");
-            else
-                MessageBox.Show("Erro email");
-
-            if (Aluno.VerificaLetras(txt_nomeAluno.Text.Trim()))
-                MessageBox.Show("Ok");
-            else
-                MessageBox.Show("Erro");*/
-            
         }
    }
 }
